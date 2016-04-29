4b368cb60193c041c10ab754a3d936d5a29aac20
Radesh Shiwlochan <radesh0430@gmail.com>
2016-04-28 23:53:25 -0400

    added a switch to change color

--- LoginSceen/ViewController.swift
+++ LoginSceen/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchOption: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func switchSwiped(sender: UISwitch) {
+        if switchOption.on {
+            view.backgroundColor = UIColor.blueColor()
+        }
+        else {
+            view.backgroundColor = UIColor.redColor()
+        }
+    }
 
 }
 
